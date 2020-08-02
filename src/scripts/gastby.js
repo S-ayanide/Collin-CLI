2a2240a2093cedbb47330ad1ce570eaebb67db00
Sayan Mondal <sayan.mondal@mayadata.io>
2020-08-02 20:51:13 +0530

    Added Mac Support
    Signed-off-by: Sayan Mondal <sayan.mondal@mayadata.io>

--- src/scripts/gastby.js
+++ src/scripts/gastby.js
@@ -1,104 +1,104 @@
-const spawnSync = require('child_process').spawnSync
+const spawnSync = require("child_process").spawnSync;
 const GatsbyNew = (appName, packageManager) => {
-  if (packageManager === 'npm') {
-    if (process.platform === 'linux') {
-      spawnSync(
-        'sudo',
-        ['npm', 'install', '--global', 'gatsby-cli'],
-        {
-          shell: true,
-          stdio: 'inherit',
-        },
-        (error, stdout, stderr) => {
-          console.log('Checking if gatsby is installed in your system')
-          if (error) {
-            console.error('stderr', stderr)
-            throw error
-          }
+	if (packageManager === "npm") {
+		if (process.platform === "linux" || process.platform === "darwin") {
+			spawnSync(
+				"sudo",
+				["npm", "install", "--global", "gatsby-cli"],
+				{
+					shell: true,
+					stdio: "inherit",
+				},
+				(error, stdout, stderr) => {
+					console.log("Checking if gatsby is installed in your system");
+					if (error) {
+						console.error("stderr", stderr);
+						throw error;
+					}
 
-          console.log(stdout)
-          console.log('Gatsby check successful')
-        },
-      )
-    } else if (process.platform === 'win32') {
-      spawnSync(
-        'npm',
-        ['i', '-g', 'gatsby-cli'],
-        {
-          shell: true,
-          stdio: 'inherit',
-        },
-        (error, stdout, stderr) => {
-          console.log('Checking if gatsby is installed in your system')
-          if (error) {
-            console.error('stderr', stderr)
-            throw error
-          }
+					console.log(stdout);
+					console.log("Gatsby check successful");
+				}
+			);
+		} else if (process.platform === "win32") {
+			spawnSync(
+				"npm",
+				["i", "-g", "gatsby-cli"],
+				{
+					shell: true,
+					stdio: "inherit",
+				},
+				(error, stdout, stderr) => {
+					console.log("Checking if gatsby is installed in your system");
+					if (error) {
+						console.error("stderr", stderr);
+						throw error;
+					}
 
-          console.log(stdout)
-          console.log('Gatsby check successful')
-        },
-      )
-    }
-  }
-  if (packageManager === 'yarn') {
-    spawnSync(
-      'npm',
-      ['i', '-g', 'yarn'],
-      {
-        shell: true,
-        stdio: 'inherit',
-      },
-      (error, stdout, stderr) => {
-        console.log('Checking if yarn is installed in your system')
-        if (error) {
-          console.error('stderr', stderr)
-          throw error
-        }
+					console.log(stdout);
+					console.log("Gatsby check successful");
+				}
+			);
+		}
+	}
+	if (packageManager === "yarn") {
+		spawnSync(
+			"npm",
+			["i", "-g", "yarn"],
+			{
+				shell: true,
+				stdio: "inherit",
+			},
+			(error, stdout, stderr) => {
+				console.log("Checking if yarn is installed in your system");
+				if (error) {
+					console.error("stderr", stderr);
+					throw error;
+				}
 
-        console.log(stdout)
-        console.log('Yarn check successful')
-      },
-    )
+				console.log(stdout);
+				console.log("Yarn check successful");
+			}
+		);
 
-    spawnSync(
-      'yarn',
-      ['glabal', 'add', 'gatsby-cli'],
-      {
-        shell: true,
-        stdio: 'inherit',
-      },
-      (error, stdout, stderr) => {
-        console.log('Checking if gatsby cli is installed in your system')
-        if (error) {
-          console.error('stderr', stderr)
-          throw error
-        }
+		spawnSync(
+			"yarn",
+			["glabal", "add", "gatsby-cli"],
+			{
+				shell: true,
+				stdio: "inherit",
+			},
+			(error, stdout, stderr) => {
+				console.log("Checking if gatsby cli is installed in your system");
+				if (error) {
+					console.error("stderr", stderr);
+					throw error;
+				}
 
-        console.log(stdout)
-        console.log('Gatsby check successful')
-      },
-    )
-  }
-  spawnSync(
-    'gatsby',
-    ['new', appName],
-    {
-      shell: true,
-      stdio: 'inherit',
-    },
-    (error, stdout, stderr) => {
-      console.log('Creating app - ', appName)
-      if (error) {
-        console.error('stderr', stderr)
-        throw error
-      }
+				console.log(stdout);
+				console.log("Gatsby check successful");
+			}
+		);
+	}
+	spawnSync(
+		"gatsby",
+		["new", appName],
+		{
+			shell: true,
+			stdio: "inherit",
+		},
+		(error, stdout, stderr) => {
+			console.log("Creating app - ", appName);
+			if (error) {
+				console.error("stderr", stderr);
+				throw error;
+			}
 
-      console.log(stdout)
-    },
-  )
-}
+			console.log(stdout);
+		}
+	);
+};
 
 module.exports = {
-  GatsbyNew,
-}
+	GatsbyNew,
+};
--- src/scripts/vue.js
+++ src/scripts/vue.js
@@ -1,62 +1,62 @@
-const spawnSync = require('child_process').spawnSync
+const spawnSync = require("child_process").spawnSync;
 const VueCreate = (appName) => {
-  if (process.platform === 'linux') {
-    spawnSync(
-      'sudo',
-      ['npm', 'install', '--global', '@vue/cli'],
-      {
-        shell: true,
-        stdio: 'inherit',
-      },
-      (error, stdout, stderr) => {
-        console.log('Checking if vue is already installed')
-        if (error) {
-          console.error('stderr', stderr)
-          throw error
-        }
+	if (process.platform === "linux" || process.platform === "darwin") {
+		spawnSync(
+			"sudo",
+			["npm", "install", "--global", "@vue/cli"],
+			{
+				shell: true,
+				stdio: "inherit",
+			},
+			(error, stdout, stderr) => {
+				console.log("Checking if vue is already installed");
+				if (error) {
+					console.error("stderr", stderr);
+					throw error;
+				}
 
-        console.log(stdout)
-      },
-    )
-  } else if (process.platform === 'win32') {
-    spawnSync(
-      'npm',
-      ['install', '-g', '@vue/cli'],
-      {
-        shell: true,
-        stdio: 'inherit',
-      },
-      (error, stdout, stderr) => {
-        console.log('Checking if vue is already installed')
-        if (error) {
-          console.error('stderr', stderr)
-          throw error
-        }
+				console.log(stdout);
+			}
+		);
+	} else if (process.platform === "win32") {
+		spawnSync(
+			"npm",
+			["install", "-g", "@vue/cli"],
+			{
+				shell: true,
+				stdio: "inherit",
+			},
+			(error, stdout, stderr) => {
+				console.log("Checking if vue is already installed");
+				if (error) {
+					console.error("stderr", stderr);
+					throw error;
+				}
 
-        console.log(stdout)
-      },
-    )
-  }
+				console.log(stdout);
+			}
+		);
+	}
 
-  spawnSync(
-    'vue',
-    ['create', appName],
-    {
-      shell: true,
-      stdio: 'inherit',
-    },
-    (error, stdout, stderr) => {
-      console.log('Creating a vue app - ', appName)
-      if (error) {
-        console.error('stderr', stderr)
-        throw error
-      }
+	spawnSync(
+		"vue",
+		["create", appName],
+		{
+			shell: true,
+			stdio: "inherit",
+		},
+		(error, stdout, stderr) => {
+			console.log("Creating a vue app - ", appName);
+			if (error) {
+				console.error("stderr", stderr);
+				throw error;
+			}
 
-      console.log(stdout)
-    },
-  )
-}
+			console.log(stdout);
+		}
+	);
+};
 
 module.exports = {
-  VueCreate,
-}
+	VueCreate,
+};
